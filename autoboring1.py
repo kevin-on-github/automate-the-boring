0f68bbc926dcb8daf8849b4f881584b3a074a29c
kevin-on-github <kevinlh@hotmail.com>
2021-08-03 11:01:31 -0400

    initial push

---
+++ autoboring1.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for name
+
+print ('Hellow world!')
+print('What is your name?')
+myname = input()
+print('nice to meet you ,' myname)
+print('name is this many characters...')
+print (len(myname))
+print('what is your age?')
+myage=input()
+print('you will be ' + str(myage) +1 + ' in a year')
